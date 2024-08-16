5f42acd4763be9519686b7802157f982b7cbf0ba
amiweb-7 <132199829+tib-dev@users.noreply.github.com>
2024-08-16 14:35:35 +0300

    Folder modification

---
+++ Amazon-clone/src/Components/Product/Product.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import ProductCard from "./ProductCard";
+import axios from "axios";
+import classes from "./product.module.css";
+import { Loader } from "../../components/Loader/Loader";
+import { productUrl } from "../Endpionts/endPoints";
+
+function Product() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${productUrl}/products`);
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <section>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={classes.product_container}>
+          {products.map((product) => (
+            <ProductCard
+              product={{ ...product, renderAdd: true }}
+              key={product.id}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Product;
