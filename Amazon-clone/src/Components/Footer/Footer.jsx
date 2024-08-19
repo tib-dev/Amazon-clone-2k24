7eb493f545a4d917cc40a8501801b95c1ad3eba0
amiweb-7 <132199829+tib-dev@users.noreply.github.com>
2024-08-19 12:00:06 +0300

    footer css

--- Amazon-clone/src/Components/Footer/Footer.jsx
+++ Amazon-clone/src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import styles from "./footer.module.css";
 import FooterMiddle from "./FooterMiddle";
 import FooterTop from "./FooterTop";
 
